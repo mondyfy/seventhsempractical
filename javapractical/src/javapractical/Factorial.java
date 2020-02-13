ef9414c8742beacde683f48211896dc3e997cc0a
developer-sandip <khaptadisandip@gmail.com>
2020-02-13 08:44:39 +0545

    add factorial

---
+++ javapractical/src/javapractical/Factorial.java
@@ -0,0 +1,12 @@
+package javapractical;
+
+public class Factorial {
+	public static void main(String args[]){  
+		  int i,fact=1;    // defining fact=1 since least value is 1
+		  int number=5;    // given input to calculate factorial
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" = "+fact);    
+		 }   
+}
--- javapractical/src/javapractical/FirstProgram.java
+++ javapractical/src/javapractical/FirstProgram.java
@@ -3,6 +3,6 @@ package javapractical;
 public class FirstProgram {
   public static void main(String args[]) {
 	// TODO Auto-generated method stub
-    System.err.println("hello");
+    System.err.println("This is first java program");
 }
 }
